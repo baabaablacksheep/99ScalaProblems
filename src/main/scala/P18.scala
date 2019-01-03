c52d1adc39be68184d6107f611ae6f29ab7db48b
Sahan Poornaka <sahanpoornaka1@gmail.com>
2019-01-04 01:07:46 +0530

    18th Problem

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -2,7 +2,7 @@
 object P18 {
 
   def getResult[A](start: Int, end: Int, list: List[A]): Unit = {
-    val slicedList: List[A] = slice(start, end, list)
+    val slicedList: List[A] = sliceTailRecursive(start, end, list)
     println(s"N Sliced List : $slicedList")
   }
 
@@ -11,11 +11,35 @@ object P18 {
   }
 
   def sliceRecursive[A](start: Int, end: Int, list: List[A]): List[A] = {
-    def sliceEnd(start: Int, end: Int, list: List[A]): List[A] = (start, end, list) match {
-      case (_, _, Nil) => Nil
-      case (_, `end` , h :: tail) => List.empty[A] //need to do something here
+    def sliceEnd(pointer: Int, list: List[A], res: List[A]): List[A] = (pointer, list) match {
+      case ( _, Nil) => res
+      case(p,h::tail)=> {
+        if(p>=start){
+          if(p<end){
+          sliceEnd(p+1,tail,res:+h)
+          }
+          else{
+            res
+          }
+        }
+        else{
+            sliceEnd(p+1,tail,res)
+          }
+        }
     }
-    sliceEnd(1,2,list)
+    sliceEnd(0,list,List())
+  }
+
+  // Tail recursive, using pattern matching.
+  def sliceTailRecursive[A](start: Int, end: Int, ls: List[A]): List[A] = {
+    def sliceR(count: Int, curList: List[A], result: List[A]): List[A] =
+      (count, curList) match {
+        case (_, Nil)                     => result.reverse
+        case (c, _ :: _) if end <= c   => result.reverse
+        case (c, h :: tail) if start <= c => sliceR(c + 1, tail, h :: result)
+        case (c, _ :: tail)               => sliceR(c + 1, tail, result)
+      }
+    sliceR(0, ls, Nil)
   }
 
 }
